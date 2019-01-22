43f5b30b58f7d5fea8bea787ccb61b188674955a
Cristina Sorell <csorell@iesmanacor.cat>
2019-01-22 11:54:58 +0100

    1a versió

--- NetBeansProjects/NetBeans1/src/netbeans1/NetBeans1.java
+++ NetBeansProjects/NetBeans1/src/netbeans1/NetBeans1.java
@@ -15,7 +15,7 @@ public class NetBeans1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola1");
+        System.out.println("Hola2");
     }
     
 }
